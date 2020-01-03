ec89b3103bd6e987e20a584aa9f82ebe5435692f
cubenoy22 <cubenoy22@gmail.com>
2020-01-04 00:15:59 +0900

    Use English

--- app/paraparax/src/ValueEditor.js
+++ app/paraparax/src/ValueEditor.js
@@ -12,13 +12,13 @@ export default class ValueEditor extends React.Component {
         overflowY: 'scroll'
       }}>
         <div>
-          <h3>プレーヤー</h3>
+          <h3>Player</h3>
           <label>
             <input
               type="checkbox"
               checked={ loop }
               onChange={ (e) => { onLoopChange(e.target.checked, undefined); } }
-            />繰り返す
+            />Loop
           </label>
           <br />
           <label>
@@ -27,7 +27,7 @@ export default class ValueEditor extends React.Component {
               checked={ reverse }
               disabled={ loopBackAndForth }
               onChange={ (e) => { onReverseChange(e.target.checked); } }
-            />リバース
+            />Reverse
           </label>
           <br />
           <label>
@@ -35,17 +35,17 @@ export default class ValueEditor extends React.Component {
               type="checkbox"
               checked={ loopBackAndForth }
               onChange={ (e) => { onLoopChange(undefined, e.target.checked); } }
-            />再生／逆再生を繰り返す
+            />Loop Back and Forth
           </label>
-          <h3>フレーム</h3>
-          <h4>再生</h4>
+          <h3>Frames</h3>
+          <h4>Playing</h4>
           delay: <input type='text' value={ this.getFrameDelay() } onChange={ this.onDelayChange.bind(this) }></input>
-          <button disabled={ !this.canDeleteFrameDelay() } onClick={ this.deleteCurrentDelay.bind(this) }>削除</button>
-          <h4>位置</h4>
-          x: <input type='text' value={ this.getFrameX() } disabled></input><br />
-          y: <input type='text' value={ this.getFrameY() } disabled></input>
-          <button disabled={ !this.canDeleteFramePosition() } onClick={ this.deleteCurrentPosition.bind(this) }>削除</button>
-          <h4>フィルター</h4>
+          <button disabled={ !this.canDeleteFrameDelay() } onClick={ this.deleteCurrentDelay.bind(this) }>Delete</button>
+          <h4>Position</h4>
+          X: <input type='text' value={ this.getFrameX() } disabled></input><br />
+          Y: <input type='text' value={ this.getFrameY() } disabled></input><br />
+          <button disabled={ !this.canDeleteFramePosition() } onClick={ this.deleteCurrentPosition.bind(this) }>Delete</button>
+          <h4>Filter</h4>
           <label><input type='range' min='0' max='100' value={ this.getFilterValue('blur') } onChange={ e => { this.onFilterChange('blur', e); } } />Blur</label>
           <label><input type='range' min='0' max='800' value={ this.getFilterValue('brightness') } onChange={ e => { this.onFilterChange('brightness', e); } } />Brightness</label>
           <label><input type='range' min='0' max='800' value={ this.getFilterValue('contrast') } onChange={ e => { this.onFilterChange('contrast', e); } } />Contrast</label>
@@ -56,7 +56,7 @@ export default class ValueEditor extends React.Component {
           <label><input type='range' min='0' max='400' value={ this.getFilterValue('saturate') } onChange={ e => { this.onFilterChange('saturate', e); } } />Saturate</label>
           <label><input type='range' min='0' max='100' value={ this.getFilterValue('sepia') } onChange={ e => { this.onFilterChange('sepia', e); } } />Sepia</label>
           <br />
-          <button disabled={ !this.canDeleteFilter() } onClick={ this.deleteCurrentFilter.bind(this) }>削除</button>
+          <button disabled={ !this.canDeleteFilter() } onClick={ this.deleteCurrentFilter.bind(this) }>Delete</button>
         </div>
       </div>
     );
@@ -100,23 +100,21 @@ export default class ValueEditor extends React.Component {
   }
 
   getFrameX() {
-    const { frames, currentIndex, timeline } = this.props;
+    const { frames, currentIndex } = this.props;
     const frame = frames[currentIndex];
     if (!frame) {
       return '';
     }
-    const hasPosition = timeline && timeline.hasPositionAt(currentIndex);
-    return `${frame.posX}${hasPosition ? '' : ' (補完)'}`;
+    return `${frame.posX}`;
   }
 
   getFrameY() {
-    const { frames, currentIndex, timeline } = this.props;
+    const { frames, currentIndex } = this.props;
     const frame = frames[currentIndex];
     if (!frame) {
       return '';
     }
-    const hasPosition = timeline && timeline.hasPositionAt(currentIndex);
-    return `${frame.posY}${hasPosition ? '' : ' (補完)'}`;
+    return `${frame.posY}`;
   }
 
   getFilterValue(key) {
